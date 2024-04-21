4f129c3210957faa68b52a36894c37d68741f3aa
GouravChamaria <gouravch794@gmail.com>
2024-04-20 20:23:31 -0400

    minor changes

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,7 +5,6 @@ import card from "./card.png";
 import coinbase from "./coinbase.png";
 import dropbox from "./dropbox.png";
 import Gourav from "./Gourav.svg";
-import profilePic from "./profile-pic.jpg";
 import quotes from "./quotes.svg";
 import robot from "./robot.png";
 import send from "./Send.svg";
