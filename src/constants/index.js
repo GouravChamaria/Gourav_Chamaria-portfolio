662db2c7d4a46063301bb315cc4f43526b4f37f5
GouravChamaria <gouravch794@gmail.com>
2024-05-23 20:02:29 -0400

    updated filecraft link

--- src/constants/index.js
+++ src/constants/index.js
@@ -211,6 +211,7 @@ export const projects = [
   {
     id: "project-1",
     title: "FileCraft.io",
+    link: "https://filecraft.io/",
     image: filecraft,
     content:
       "Filecraft offers an unparalleled file conversion experience by seamlessly merging speed, accuracy, and user-friendliness",
